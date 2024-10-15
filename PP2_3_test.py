53a810d7df0e53d619c9e2765b04f27e4c868772
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-10-15 09:55:49 -0400

    Update PP2_3_test.py

--- PP2_3_test.py
+++ PP2_3_test.py
@@ -10,7 +10,7 @@ def test_q1_1(capsys):
 	except:
 		sys.exit()
 
-	input_values = ['5', '5', '2']
+	input_values = ['knife']
 
 	def mock_input(s):
 		print(s, end='')
@@ -19,9 +19,161 @@ def test_q1_1(capsys):
 
 	PP2_3.q1()
 	captured = capsys.readouterr()
-	assert captured.out == "Input a number: Input a number: Input a number: Isosceles\n"
+	assert captured.out == "In: -ives\n"
+
+def test_q2_1(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['10']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "In: 10 is positive\n"
 
 def test_q1_2(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['hey']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "In: -eys\n"
+
+def test_q2_2(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['0']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "In:\n"
+
+def test_q1_3(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['helpp']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "In: -s\n"
+
+def test_q2_3(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['-3']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "In: -3 is negative\n"
+
+def test_q1_4(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['Daisy']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "In: -ies\n"
+
+def test_q2_4(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['5']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "In: 5 is positive\n"
+
+def test_q3_1(capsys):
+
+	try:
+		exists = os.path.exists("PP2_3.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['5', '5', '2']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_3.input = mock_input
+
+	PP2_3.q3()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Input a number: Input a number: Isosceles\n"
+
+def test_q3_2(capsys):
 
 	try:
 		exists = os.path.exists("PP2_3.py")
@@ -36,11 +188,11 @@ def test_q1_2(capsys):
 		return input_values.pop(0)
 	PP2_3.input = mock_input
 
-	PP2_3.q1()
+	PP2_3.q3()
 	captured = capsys.readouterr()
 	assert captured.out == "Input a number: Input a number: Input a number: Scalene\n"
 
-def test_q1_3(capsys):
+def test_q3_3(capsys):
 
 	try:
 		exists = os.path.exists("PP2_3.py")
@@ -55,11 +207,11 @@ def test_q1_3(capsys):
 		return input_values.pop(0)
 	PP2_3.input = mock_input
 
-	PP2_3.q1()
+	PP2_3.q3()
 	captured = capsys.readouterr()
 	assert captured.out == "Input a number: Input a number: Input a number: No Triangle\n"
 
-def test_q1_4(capsys):
+def test_q3_4(capsys):
 
 	try:
 		exists = os.path.exists("PP2_3.py")
@@ -74,7 +226,7 @@ def test_q1_4(capsys):
 		return input_values.pop(0)
 	PP2_3.input = mock_input
 
-	PP2_3.q1()
+	PP2_3.q3()
 	captured = capsys.readouterr()
 	assert captured.out == "Input a number: Input a number: Input a number: Equilateral\n"
 
